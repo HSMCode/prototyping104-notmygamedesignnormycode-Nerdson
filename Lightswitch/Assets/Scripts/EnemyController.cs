515ee182ccdf01d936fe7027d003e39b4d25d581
Toni <black_jack_21@gmx.de>
2020-12-10 17:57:30 +0100

    add everything... sorry
    Hatte das falsche Repo geöffnet, worauf ich keinen Zugriff fürs Pushen hatte... also musste ich jetzt alles auf einmal hier ins richtige Repo pushen

---
+++ Lightswitch/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+            myRigidbody.MovePosition(temp);
+        }
+    }
+}
---
+++ Lightswitch/Assets/Scripts/GhostLaughScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostLaughScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            SoundScript.PlaySound2();
+        }
+    }
+}
